73909772ef3866e29f916e29db60b0b12b8b0887
SreeNath <gootysreenathreddy@gmail.com>
2023-12-21 23:35:56 +0530

    Move zeros to end in list

---
+++ movingZeros.py
@@ -0,0 +1,15 @@
+def moveZeros(nums):
+    j = 0
+    for i in range(len(nums)):
+        if nums[i]!=0:
+            nums[j]=nums[i]
+            j +=1
+
+    for k in range(j,len(nums)):
+        nums[k]=0
+    return nums
+
+
+num=[6,7,0,9,0,3,3,4,5,0]
+
+print(moveZeros(num))
