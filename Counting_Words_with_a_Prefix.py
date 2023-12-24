3d07c6908db05ffbac949ee929c9425383370d7b
SreeNath(He/Him) <gootysreenathreddy@gmail.com>
2023-12-24 12:07:37 +0530

    Add files via upload

---
+++ Counting_Words_with_a_Prefix.py
@@ -0,0 +1,11 @@
+words=["bilingual", "macroeconomics", "enlarge", "macroscopic", "monolingual"]
+prefix="macro"
+counter=0
+for i in range(len(words)):
+    prefix_count=words[i][:len(prefix)]
+    if prefix_count==prefix:
+        counter +=1
+print("print the prefix word:",counter)
+
+#output:-
+print the prefix word: 2
