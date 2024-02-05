768685c85acd01c63c6d0ad401d42a83917c30b3
SreeNath <gootysreenathreddy@gmail.com>
2024-02-06 00:39:13 +0530

    Add files via upload

---
+++ min_and_max.py
@@ -0,0 +1,23 @@
+List=[3,5,9,10,15,3,2,1,7]
+
+max_value=List[0]
+
+for x in List:
+
+    if x > max_value:
+        max_value=x
+print(max_value)
+
+minimum value in the given list
+
+min_value=List[0]
+
+for x in List:
+
+    if x < max_value:
+        max_value=x
+print(max_value)
+
+
+
+
