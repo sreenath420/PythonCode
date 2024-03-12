7e79a5e72921841a905781ae8f1198c0c671fa71
SreeNath <gootysreenathreddy@gmail.com>
2024-03-12 23:07:43 +0530

    Update python_pattern.py

--- python_pattern.py
+++ python_pattern.py
@@ -674,6 +674,18 @@ step:-4 i is here 4
    
                       1 2 3 4
    
-   
+ ----------------------------------------- program 19 ------------------------------------------------  
 
-		 
+   *     
+  *   *   
+*       * 
+  *   *   
+    *     
+for i in range(5):
+    for j in range(5):
+        if  i+j ==2 or i-j==2 or i +j==6 or j-i==2:
+            print("*",end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+
