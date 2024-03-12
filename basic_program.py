a84ec3ecbcec4300c4132489006961ebf1a8158e
SreeNath <gootysreenathreddy@gmail.com>
2024-03-12 22:47:20 +0530

    Add files via upload

---
+++ basic_program.py
@@ -0,0 +1,14 @@
+try:
+    num = int(input('Enter Number\t'))
+    factorial = 1
+    if num == 0:
+        print("o/p is 1")
+    elif num < 0:
+        print("not exist")
+    else:
+        for i in range(1, num + 1):
+            factorial = factorial * i
+        print(factorial)
+
+except ValueError:
+    print("Error:Please enter a Valid Number")
