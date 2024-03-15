d96a6367bfa6dbe8fd78c134e3f8888c03abe676
SreeNath <gootysreenathreddy@gmail.com>
2024-03-15 22:33:49 +0530

    Update basic_program.py

--- basic_program.py
+++ basic_program.py
@@ -14,3 +14,32 @@ try:
 
 except ValueError:
     print("Error:Please enter a Valid Number")
+
+
+
+--------------------------------------------------------->Fib number<-----------------------------------------------------------------------
+
+try:
+
+    n = int(input("Enter a Number"))
+    a = 0
+    b = 1
+
+    if n == 0:
+        print("not egligble for fib number")
+    elif n == 1:
+        print("Fib number is", b)
+
+    else:
+        count = 0
+        while count < n:
+            print(a)
+            n1 = a + b
+            a = b
+            b = n1
+            # n = a + b]
+            count +=1
+except ValueError:
+    print("Enter the valid number")
+
+
