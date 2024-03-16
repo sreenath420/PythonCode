cedba210339814759e0e2c485e5cf69e1d2d0c91
SreeNath <gootysreenathreddy@gmail.com>
2024-03-17 00:51:30 +0530

    Update Python_Interview.py

--- Python_Interview.py
+++ Python_Interview.py
@@ -46,6 +46,45 @@ print(counter)
 o/p
 {'f': 1, 'd': 1, 'c': 1, 'a': 7, 'g': 1, 'h': 1, 'j': 1, 'q': 1}
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+5.Find the largest pair sum in an array in python by using for loop
+Method:-1
+def largest_pair_sum(arr):
+    # Initialize variables to store the largest and second-largest elements
+    max1 = 0
+    max2 = 0
+
+    # Iterate through the array to find the largest and second-largest elements
+    for num in arr:
+        if num > max1:
+            max2 = max1
+            max1 = num
+        elif num > max2:
+            max2 = num
+
+    # Return the sum of the largest and second-largest elements
+    return max1, max2
+
+# Example usage:
+array = [3, 6, 2, 8, 10, 5,15]
+max_pair_sum = largest_pair_sum(array)
+print("Largest pair sum:", max_pair_sum)
+
+Method:-2
+def largest_pair_sum(arr):
+    # Sort the array in ascending order
+    arr.sort()
+    print(arr[-1],arr[-2])
+    # Return the sum of the last two elements
+    return arr[-1],arr[-2]
+
+
+# Example usage:
+array = [3, 6, 2, 8, 10, 5,15]
+max_pair_sum = largest_pair_sum(array)
+print("Largest pair sum:", max_pair_sum)
+
+
 
 
 
