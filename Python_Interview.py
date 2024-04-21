c0a37ac86bbd7d59b74c9d3b94c0cc42dac935d6
SreeNath <gootysreenathreddy@gmail.com>
2024-04-21 23:02:04 +0530

    Update Python_Interview.py

--- Python_Interview.py
+++ Python_Interview.py
@@ -155,5 +155,27 @@ True
 
 
 
+----------------------------------------------------------------->is_subsequence<---------------------------------------------------------------------------
 
+def is_subsequence(s,t):
+    i=j=0
+    while i < len(s) and j < len(t):
+        if s[i]==t[j]:
+
+            #print(s[i],t[j])
+            i += 1
+
+
+        j +=1
+
+    return i==len(s)
+
+s1 = "abc"
+s2 = "sagfhdbdsrgcadsf"
+s3="abc"
+s4="sgfhdbdsrgcadsf"
+print(is_subsequence(s1,s2))
+
+True
+False
 
