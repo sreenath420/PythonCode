1997852adeb120ef166f9f6b9c39022134e72c36
SreeNath <gootysreenathreddy@gmail.com>
2024-05-30 23:06:54 +0530

    Update Counting_Words_with_a_Prefix.py

--- Counting_Words_with_a_Prefix.py
+++ Counting_Words_with_a_Prefix.py
@@ -5,7 +5,7 @@ for i in range(len(words)):
     prefix_count=words[i][:len(prefix)]
     if prefix_count==prefix:
         counter +=1
-print("print the prefix word:",counter)
+print("print the prefixing word:",counter)
 
 #output:-
 print the prefix word: 2
