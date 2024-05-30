39132865ad768437951006b76736e74c1bbb85a9
SreeNath <gootysreenathreddy@gmail.com>
2024-05-30 23:55:04 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -187,3 +187,15 @@ def get_nested_values(nested_dict,keys):
     return nested_dict
 
 print(get_nested_values(nested_dict,keys))
+
+Define a function with takes two dictionaries and merge them and sum the values of common keys.
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'b': 3, 'c': 4, 'd': 5}
+
+def merge_dict(dict1,dict2):
+    for k,v in dict2.items():
+        dict1[k] = dict1.get(k,0) + v ## to dict1, adding k from for loop
+    return dict1
+
+output = merge_dict(dict1,dict2)
+print("Sum of similar keys in dictionary",output)
