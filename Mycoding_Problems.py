33788faed1ffa8a8837e3616e5ba89a10ab797d2
SreeNath <gootysreenathreddy@gmail.com>
2024-06-18 21:51:37 +0530

    Update Mycoding_Problems.py

--- Mycoding_Problems.py
+++ Mycoding_Problems.py
@@ -285,5 +285,21 @@ o/p
 [0, 5, 17]
 
 
+---------------------------------------------->Prime_Number<----------------------------------------------------------------
+
+#checking given number is prime or not
+given_number = 9999991
+
+def primeNumber(givnum):
+    strout = ""
+    for i in range(2,givnum):
+       if givnum % i == 0:
+           strout = "Given is not a prime"
+           break
+    else:
+        strout = "Given is prime"
+    return strout
+
+print(primeNumber(given_number))
 
 
