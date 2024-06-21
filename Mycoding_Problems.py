54ed89e0fdc5aa063308b3c7244a263d32301ebd
SreeNath <gootysreenathreddy@gmail.com>
2024-06-21 23:59:15 +0530

    Update Mycoding_Problems.py

--- Mycoding_Problems.py
+++ Mycoding_Problems.py
@@ -313,8 +313,12 @@ def flattenStrings(lst): ## it will flattten integer and string both
     for i in lst:
         if isinstance(i,list):
             flatten_list.extend(i)
+            #exted will add antoher list here its comes [1, 2, 3, 4, 5, 6, 7, 8, 9]
         else:
             flatten_list.append(i)
     return flatten_list
 
 print(flattenStrings(lst))
+
+output:-
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 10, 11]
