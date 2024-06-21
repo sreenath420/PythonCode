e1027e5de3c24b03e49ef04c1e9be0f68246b567
SreeNath <gootysreenathreddy@gmail.com>
2024-06-21 23:50:57 +0530

    Update Mycoding_Problems.py

--- Mycoding_Problems.py
+++ Mycoding_Problems.py
@@ -303,3 +303,18 @@ def primeNumber(givnum):
 print(primeNumber(given_number))
 
 
+------------------------------------> flattend list<-------------------------------------------------------------
+
+#Flatten nested list
+lst = [[1,2,3,4,5],[6,7,8,9],'a','b',10,11]
+flatten_list = []
+
+def flattenStrings(lst): ## it will flattten integer and string both
+    for i in lst:
+        if isinstance(i,list):
+            flatten_list.extend(i)
+        else:
+            flatten_list.append(i)
+    return flatten_list
+
+print(flattenStrings(lst))
