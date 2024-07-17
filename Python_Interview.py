59d23f055f37e4fea622f1f39c916fd7a3a3953a
SreeNath <gootysreenathreddy@gmail.com>
2024-07-18 01:34:35 +0530

    Update Python_Interview.py

--- Python_Interview.py
+++ Python_Interview.py
@@ -378,3 +378,11 @@ for k,v in nested_dict.items():
         #print(j)
         for k,x in j.items():
             print(x)
+
+-------------------------------------->print common values between list<----------------------------------------------
+list1 = [1, 2, 3, 4]
+list2 = [3, 4, 5, 6]
+out=[3,4]
+
+result=[x for x in list1 if x in list2]
+print(result)
