46547bdf442bec44ea1ba14f0ef0fc9de930e668
SreeNath <gootysreenathreddy@gmail.com>
2024-08-02 22:52:03 +0530

    Update movingZeros.py

--- movingZeros.py
+++ movingZeros.py
@@ -1,3 +1,4 @@
+#method:-1
 def moveZeros(nums):
     j = 0
     for i in range(len(nums)):
@@ -14,6 +15,18 @@ num=[6,7,0,9,0,3,3,4,5,0]
 
 print(moveZeros(num))
 
+#method:-2
 New_list= [num for num list if list!=0]
 New= [num for num list if list=0]
 Print(new_list+ New)
+
+#method:-3
+
+def moveZeros(Num):
+    r=0
+    for i in range(len(Num)):
+        if Num[i]:  #here will check if i = 0 returns false and swaping happens basically if 0: returns false
+            Num[r],Num[i] = Num[i],Num[r]
+            r += 1
+    return Num
+print(moveZeros(Num))
