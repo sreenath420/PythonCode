d655e1374b19a341017a3a6841ee4b6370a90a2e
SreeNath <gootysreenathreddy@gmail.com>
2025-12-25 20:10:07 +0530

    Create Interview_programs.py

---
+++ Praveen&Sreenath_Prep/Interview_programs.py
@@ -0,0 +1,14 @@
+STRING
+---------------------------------------->1.Program<------------------------------------------------
+
+s= "DataEngineering"
+
+#Q1. Reverse the string using a single line command; hint - via slicing
+
+
+#Q2. Reverse the string without using single line command; hint - via a loop
+
+result=''
+for i in s:
+  result =i + result
+print(result)
